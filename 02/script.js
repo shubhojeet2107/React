10877622ef3b1d22aa403efc56ba73803327313e
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2026-02-09 14:57:17 +0530

    learning about parcel

---
+++ 02/script.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading3 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from React!!!"
+);
+
+// log(heading3) -> this will return a JS object.
+const root3 = ReactDOM.createRoot(document.getElementById("root"));
+// root3.render(heading3);
+
+
+
+/*
+ <div id="parent">
+     <div id="child">
+         <h1>I am h1 tag </h1>
+     </div>
+ </div>
+*/
+const heading4 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am h1 tag")
+  )
+);
+// root3.render(heading4);
+
+
+
+/*
+ <div id="parent">
+     <div id="child">
+         <h1>I am h1 tag </h1>
+         <h2>I am h2 tag </h2>
+     </div>
+ </div>
+*/
+const heading5 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+// root3.render(heading5);
---
+++ README.md
@@ -0,0 +1,16 @@
+# REACT 📚
+
+# Parcel
+- Dev Build
+- Local Server
+- Automatically renders the brpwser page
+- File watching algorithm -> written in C++
+- Faster builds because of caching
+- Image optimization
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting (basically spiltting the files)
+- Differential bundling -> supports old browsers
+- Better error suggestions
+- Different bundles for dev and production  
